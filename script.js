14ea0c8efc98862be809d90619c16f8123f24b37
jakeburns <jakeburns@MacBook-Pro-4.local>
2019-04-02 15:04:15 -0400

    svg fix

--- script.js
+++ script.js
@@ -4,19 +4,23 @@ data.then(function(data){drawChart(data.kittens);})
 
 var drawChart = function(data)
 {
-
+  console.log("Data:", data)
   var screen = {height: 500, width: 500}
   var margins = {top:10,bottom:10,left:10,right:10}
 
+ var svg = d3.select("svg")
+                  .attr("height",screen.height)
+                  .attr("width",screen.width);
+                    
   var xScale = d3.scaleLinear()
                  .domain([0,function(d){return d.length;}])
-                 .range([0,8])
+                 .range([0,8]);
 
   var yScale = d3.scaleLinear()
                  .domain([0,d3.max(function(d){return d;})])
                  .range([screen.height,0]);
-   var svg = d3.select("svg")
-           .selectAll("circle")
+
+           svg.selectAll("circle")
            .datum(data)
            .enter()
            .append("circle")
