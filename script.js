5af7f806f8349e338cdaef667137967bf0eaa292
jakeburns <jakeburns@MacBook-Pro-4.local>
2019-04-02 20:00:02 -0400

    recent

--- script.js
+++ script.js
@@ -13,32 +13,68 @@ var drawChart = function(data)
 
  var svg = d3.select("svg")
                   .attr("height",screen.height)
-                  .attr("width",screen.width);
+                  .attr("width",screen.width)
+                  .on("click", function(){
+
+                  })
 
   var xScale = d3.scaleLinear()
-                 .domain([0,function(d){return d.length;}])
-                 .range([0,8]);
+                 .domain([0, data.length])
+                 .range([0,width]);
 
   var yScale = d3.scaleLinear()
-                 .domain([0,d3.max(function(d){return d;})])
+                 .domain([0,d3.max(data)])
                  .range([height,0]);
 
            svg.selectAll("circle")
            .data(data)
            .enter()
            .append("circle")
-           .attr("cy", function(d){return yScale(d);})
-           .attr("cx", function(d,i){return xScale(i);})
-           .attr("r", 2)
+           .attr("cy", function(d){return yScale(d) + 5;})
+           .attr("cx", function(d,i){return xScale(i) + 30;})
+           .attr("r", 2);
+
+           var area = d3.area()
+                        .x(function(d,i){return xScale(i);})
+                        .y0(function(d){return yScale(height);})
+                        .y1(function(d){return yScale(height-d);})
+                        //.curve(d3.curveCatmullRom);
+
+                        svg.append("path")
+                         .data(data)
+                         .attr("class", "area")
+                         .attr("d", area)
+                         .attr("stroke-width", "2")
+                         .attr("fill", "blue")
 
     var line = d3.line()
-                 .x(function(d,i){return xScale(i);})
-                 .y(function(d){return yScale(d);})
-                 .curve(d3.curveCatmullRom)
+                 .x(function(d,i){return xScale(i) + 30;})
+                 .y(function(d){return yScale(d)+ 5;});
+                 //.curve(d3.curveCatmullRom)
 
-    var area = d3.area()
-                 .x(function(d,i){return xScale(i);})
-                 .y0(function(d){return yScale(height);})
-                 .y1(function(d){return yScale(height-d);})
-                 .curve(d3.curveCatmullRom)
+                 svg.append("path")
+                 .datum(data)
+                 .attr("class", "line")
+                 .attr("d", line)
+                 .attr("fill", "none")
+                 .attr("stroke", "black");
+
+
+                 var yAxis = d3.axisLeft(yScale)
+         .ticks(9)
+         .tickSize(10)
+svg.append("g").classed("yAxis",true)
+.call(yAxis)
+.attr('transform', 'translate(' + (margins.left +10)+ ','+(margins.top + 1)+')');
+
+var xAxis = d3.axisBottom(xScale)
+    .ticks(data.length)
+    .tickFormat(function(d,i){
+      console.log(d, "i", i);
+
+    })
+    .tickSize(1)
+    svg.append("g").classed("xAxis",true)
+    .call(xAxis)
+    .attr('transform', 'translate(' + (margins.left + 10)+ ','+(height - margins.top+5)+')');
 }
